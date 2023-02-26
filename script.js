c2386f0f56985ce8ac9145c983685d93984e2ded
Edward Idema <36839396+SilentEddie@users.noreply.github.com>
2023-02-26 12:13:21 +0100

    improved effect, added second text to try different versions

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
-let text = ["Hello", "I", "am", "a", "beautiful", "animated", "text"];
-const interval = 1800;
+//let text = ["Hello", "I", "am", "a", "beautiful", "animated", "text"];
+let text = ["Goedemiddag", "en", "welkom", "bij", "de", "monthly", "van", "februari"];
+const interval = 750;
+const wait = 200;
+const text1 = document.getElementById("blobText1");
+const text2 = document.getElementById("blobText2");
+const text3 = document.getElementById("blobText3");
+const text4 = document.getElementById("blobText4");
+
+const elementArray = [text1, text2, text3, text4];
+
 
 start();
 
 function start() {
-  animateText(text);
+  setTimeout(function() {
+    toggleBlobby();
+  }, interval/2);
+  setTimeout(function() {
+    toggleBlobby();
+  }, interval*0.9);
+  setTimeout(function() {
+    placeAllWords();
+  }, interval+interval*1.2);
+}
+
+function placeAllWords() {
+  elementArray[1].classList.add("blobby");
+  elementArray[3].classList.add("blobby");
+
+  for (let i = 0; i < text.length; i++) {
+    let word = text[i];
+    let element = elementArray[i%2];
+    let element2 = elementArray[i%2+2];
+    setTimeout( function() {
+      setWordToElement(word,element,element2)
+    }, i*interval*2)
+    
+  }
+}
+
+
+function setWordToElement(word, ...elements) {
+  elements.forEach(function(element) {
+    element.innerText = word;
+  });
+  toggleBlobby();
+}
+
+function toggleBlobby() {
+  elementArray.forEach(function(element) {
+    element.classList.toggle("blobby")
+  });
 }
 
 function animateText(text) {
@@ -16,7 +62,7 @@ function animateText(text) {
   deblobbify();
   setTimeout(function() {
     animateText(text);
-  }, interval*2);
+  }, interval*2+wait);
 }
 
 function animatedWord(word) {
@@ -27,8 +73,17 @@ function animatedWord(word) {
 }
 
 function setNewWordOnBackground(word) {
-  console.log("setNewWordOnBackground");
-  document.getElementById("blobText2").innerText = word;
+  setTimeout(function() {
+    console.log("setNewWordOnBackground");
+    document.getElementById("blobText2").innerText = word;
+  }, interval);
+}
+
+function setNewWordOnForeground(word) {
+
+    console.log("setNewWordOnForeground");
+    document.getElementById("blobText1").innerText = word;
+
 }
 
 function blobbify() {
@@ -39,6 +94,8 @@ function blobbify() {
 function swapWordsBackGroundAndForeground() {
   setTimeout(function() {
     console.log("swap");
+    console.log(document.getElementById("blobText1").innerText)
+    console.log(document.getElementById("blobText2").innerText)
     let temp = document.getElementById("blobText1").innerText;
     document.getElementById("blobText1").innerText = document.getElementById("blobText2").innerText;
     document.getElementById("blobText2").innerText = temp;
@@ -49,5 +106,5 @@ function deblobbify() {
   setTimeout(function() {
     console.log("deblobbify");
     document.getElementById("blobText-wrapper").classList.remove("blobbified");
-  }, interval+20);
+  }, interval);
 }
